ef46595e46dbda15dbe09cd9b0be34feba7db5c8
persian-123 <rohamrahimi22@gmail.com>
2025-01-10 18:34:34 +0000

    OOP structure is setup

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://roham:Roham137789@study-planner-db.c3k2akqmsl3v.us-east-1.rds.amazonaws.com:5432/study-planner-db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = 'your-very-secret-key'
